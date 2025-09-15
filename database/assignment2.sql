71beedbff85c668037966dae0f9f6e232e3e03a5
Alejandro Malvacias <alejandromalvacias1@gmail.com>
2025-09-15 09:53:42 -0400

    Sql assignment

---
+++ database/assignment2.sql
@@ -0,0 +1,32 @@
+-- #1 Tony Stark
+
+INSERT INTO account (account_firstname, account_lastname, account_email, account_password)
+VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
+
+
+
+-- #2 UPDATE Tony
+UPDATE account
+SET account_type = 'Admin'
+WHERE account_id = <ID_TONY>;
+
+-- #3 Delete Tony
+DELETE FROM account
+WHERE account_id = <ID_TONY>;
+
+-- #4 UPDATE 
+UPDATE inventory
+SET inv_description = REPLACE (inv_description, 'small interiors', 'huge interiors')
+WHERE inv_make = 'GM' AND inv_model = 'Hummer';
+
+-- #5 SELECT
+SELECT i.inv_make, i.inv_model, c.classification_name
+FROM inventory i
+JOIN classification c ON i.classification_id = c.classification_id
+WHERE c.classification_name = 'Sport'
+
+-- #6 UPDATE
+UPDATE inventory
+SET 
+    inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
